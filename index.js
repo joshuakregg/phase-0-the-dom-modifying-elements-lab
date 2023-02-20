a2762a1c2e35e76f26f08d0fb315dc73c3895e75
joshau kregg <kreggjoshua@gmail.com>
2023-02-20 06:37:03 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementsByTagName("main");
+main[0].remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Joshua is the champion";
+document.body.append(newHeader);
